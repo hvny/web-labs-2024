600cf47f1bc03e8ff6c4ad4f9a38d288b6a6b693
Vadim Elsukov <elsukov.vadim@gmail.com>
2024-02-19 23:27:08 +0300

    task1 остались только стили

--- laba16/task1/index.js
+++ laba16/task1/index.js
@@ -1,8 +1,22 @@
 const productsList = document.querySelector("#products");
 const form = document.querySelector(".form");
+const nameInput = form.elements.nameInput;
+const priceInput = form.elements.priceInput;
+const listItemTemplate = document.querySelector('#product');
 
+function addItem(name, price) {
+    listItem = listItemTemplate.content.cloneNode(true);
+    listItem.querySelector(".products-list__product-name").textContent = name;
+    listItem.querySelector(".products-list__product-price").textContent = price;
+    listItem.querySelector(".products-list__button").addEventListener("click", (evt) => {
+        evt.target.closest("li").remove();
+    });
+    productsList.append(listItem);
+}
 
 
 form.addEventListener("submit", (evt) => {
     evt.preventDefault();
+    console.log(nameInput.value, priceInput.value);
+    addItem(nameInput.value, priceInput.value);
 });
