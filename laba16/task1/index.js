066c72523ad4dca6eac63b5e46446cde9cd0cfef
Vadim Elsukov <elsukov.vadim@gmail.com>
2024-02-19 23:38:17 +0300

    microfix

--- laba16/task1/index.js
+++ laba16/task1/index.js
@@ -4,8 +4,13 @@ const nameInput = form.elements.nameInput;
 const priceInput = form.elements.priceInput;
 const listItemTemplate = document.querySelector('#product');
 
+
+function generateProductItem() {
+    return listItemTemplate.content.cloneNode(true);
+}
+
 function addItem(name, price) {
-    listItem = listItemTemplate.content.cloneNode(true);
+    const listItem = generateProductItem();
     listItem.querySelector(".products-list__product-name").textContent = name;
     listItem.querySelector(".products-list__product-price").textContent = price;
     listItem.querySelector(".products-list__button").addEventListener("click", (evt) => {
@@ -17,6 +22,5 @@ function addItem(name, price) {
 
 form.addEventListener("submit", (evt) => {
     evt.preventDefault();
-    console.log(nameInput.value, priceInput.value);
     addItem(nameInput.value, priceInput.value);
 });
