5fa5d12bf5113914982c9c71c7b409d00a23695d
Vadim Elsukov <elsukov.vadim@gmail.com>
2024-02-19 14:22:50 +0300

    laba16 start

---
+++ laba16/task1/index.js
@@ -0,0 +1,8 @@
+const productsList = document.querySelector("#products");
+const form = document.querySelector(".form");
+
+
+
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+});
